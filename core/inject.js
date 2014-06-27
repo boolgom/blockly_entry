121fc234d7fb88d7c768b24ac0cc8c56740ef6f1
Kyumin Sim <kyumin92@gmail.com>
2014-06-27 20:27:10 +0900

    adddddddddsomething

--- core/inject.js
+++ core/inject.js
@@ -285,9 +285,15 @@ Blockly.createDom_ = function(container) {
                 block.moveBy(overflow, 0);
               }
               // Delete any block that's sitting on top of the flyout.
-              if (block.isDeletable() && (Blockly.RTL ?
+              if (typeof(Entry) == "object" && block.isDeletable() && (Blockly.RTL ?
                   blockXY.x - 2 * metrics.viewLeft - metrics.viewWidth :
                   -blockXY.x) > + Blockly.mainWorkspace.scrollX) {
+                block.dispose(false, true);
+              }
+
+              if (typeof(Entry) != "object" && block.isDeletable() && (Blockly.RTL ?
+                  blockXY.x - 2 * metrics.viewLeft - metrics.viewWidth :
+                  -blockXY.x) > + Blockly.mainWorkspace.scrollX - 150) {
                 block.dispose(false, true);
               }
             }
